eb8ea0d83f5b4786b6505b88af4476817605adc4
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2022-07-27 22:40:27 +0200

    - option to include JUnit - fix project encoding - polish

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,15 +1,17 @@
 import os
 
-directory = '{{ cookiecutter.groupId.replace('.','/') + '/' + cookiecutter.artifactId }}'
+directory = '{{ cookiecutter.__package.replace('.','/') }}'
 srcDir = 'src/main/java/' + directory
 testDir = 'src/test/java/' + directory
 
 os.makedirs(srcDir, exist_ok=True)
-os.makedirs(testDir, exist_ok=True)
+
+if '{{ cookiecutter.junit }}' == 'yes':
+	os.makedirs(testDir, exist_ok=True)
 
 os.rename("Main.java", srcDir + '/Main.java')
 
 os.system('mvn wrapper:wrapper')
 
-os.system('./mvnw package')
+os.system('./mvnw verify')
 
